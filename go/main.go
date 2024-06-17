f084c6492e3c8893a6fb1a61f6956d67f4f0c5cc
root <root@terraform-instance.asia-northeast1-a.c.terraform-project-425615.internal>
2024-06-17 15:35:01 +0000

    infra setting

--- go/main.go
+++ go/main.go
@@ -364,7 +364,7 @@ func main() {
 		http.FileServer(http.Dir("../public")).ServeHTTP(w, r)
 	})
 
-	log.Fatal(http.ListenAndServe(":80", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func getSession(r *http.Request) *sessions.Session {
